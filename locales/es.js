0f01646ae998853691f5df6827dbb57347897a12
Facundo Perez Montalvo <facuperezm@facundos-mbp.home>
2022-10-30 12:36:23 -0300

    updated db with actual prices and translations

--- locales/es.js
+++ locales/es.js
@@ -19,22 +19,22 @@ export default {
       {
         name: "PAPAS FRITAS",
         description: "",
-        price: "10000",
+        price: "700",
       },
       {
         name: "PAPAS FRITAS CON CHEDDAR Y BACON",
         description: "",
-        price: "10000",
+        price: "800",
       },
       {
         name: "DUO DE EMPANADAS FRITAS (CARNE O JYQ)",
         description: "",
-        price: "10000",
+        price: "400",
       },
       {
         name: "MANDIOCA FRITA CON PROVENZAL",
         description: "",
-        price: "10000",
+        price: "700",
       },
     ],
     picadas: [
@@ -42,86 +42,98 @@ export default {
         name: "PICADA HERALDO",
         description:
           "Variedad de quesos y fiambres, aceitunas, mandioca frita, berenjenas en escabeche y pan",
-        price: "10000",
+        price: "2400",
       },
       {
         name: "PICADA MAR Y TIERRA",
         description:
           "Rabas rebozadas, camarones panados, bastones de merluza, salamín, queso, milanesa, mandioca frita y aceitunas",
-        price: "10000",
+        price: "2800",
       },
       {
         name: "POLLO FRITO",
         description: "Alitas de pollo acompañadas con papas fritas",
-        price: "10000",
+        price: "1700",
       },
     ],
     entrepanes: [
       {
         name: "SHAWARMA VEGGIE",
         description: "Pan árabe, vegetales, mayonesa de ajo",
-        price: "10000",
+        price: "1000",
       },
       {
         name: "SHAWARMA CON VEGETALES, CARNE O POLLO",
         description: "Pan árabe, vegetales, mayonesa de ajo, carne o pollo",
-        price: "10000",
+        price: "1200",
       },
       {
         name: "HAMBURGUESA SIMPLE",
         description: "Queso cheddar, lechuga, tomate y mayonesa",
-        price: "10000",
+        price: "1100",
       },
       {
         name: "HAMBURGUESA BLUE",
         description: "Queso azul, rúcula y mayonesa",
-        price: "10000",
+        price: "1200",
       },
       {
         name: "HAMBURGUESA COMPLETA",
         description:
           "Queso cheddar, huevo, panceta, lechuga, tomate, cebolla y mayonesa",
-        price: "10000",
+        price: "1300",
+      },
+      {
+        name: "HAMBURGUESA CON QUESO",
+        description:
+          "Queso cheddar",
+        price: "1000",
+      },
+      {
+        name: "ADICIONAL DE MEDALLÓN",
+        description:
+          "",
+        price: "200",
       },
     ],
     adicionales: [
       {
         name: "MAYONESA DE AJO",
         description: "",
-        price: "10000",
+        price: "150",
       },
       {
         name: "SALSA PROVENZAL",
         description: "",
-        price: "10000",
+        price: "150",
       },
     ],
     alplato: [
       {
         name: "MILANESA DE TERNERA CON FRITAS",
         description: "",
-        price: "10000",
+        price: "1350",
       },
       {
         name: "MILANESA DE TERNERA A CABALLO CON FRITAS",
         description: "",
-        price: "10000",
+        price: "1450",
       },
       {
         name: "MILANESA A LA NAPOLITANA CON FRITAS",
         description: "",
-        price: "10000",
+        price: "1550",
       },
       {
         name: "PESCA DEL DIA",
         description: "Pacú o boga con vegetales grillados",
-        price: "10000",
+        price: "2400",
       },
       {
         name: "PICANHA",
         description:
           "Picanha cortada con sal y láminas de ajo, acompañada de mandioca frita",
-        price: "10000",
+        price: "2500",
       },
     ],
     ensaladas: [
@@ -129,79 +141,79 @@ export default {
         name: "MULTICOLOR",
         description:
           "Hojas verdes, tomate, cebolla, zanahoria, arvejas, cholo, huevo duro.",
-        price: "10000",
+        price: "900",
       },
       {
         name: "VERDE",
         description: "Rúcula, lechuga, queso azul, tomates cherry",
-        price: "10000",
+        price: "900",
       },
       {
         name: "CÉSAR",
         description: "Lechuga, pollo, crutones",
-        price: "10000",
+        price: "950",
       },
     ],
     delmar: [
       {
         name: "RABAS REBOZADAS",
         description: "",
-        price: "10000",
+        price: "1900",
       },
       {
         name: "CAMARONES PANADOS",
         description: "",
-        price: "10000",
+        price: "2000",
       },
     ],
     pizzas: [
       {
         name: "CLÁSICA MUZZARELA",
         description: "Salsa pizza, queso muzzarella",
-        price: "10000",
+        price: "950",
       },
       {
         name: "RÚCULA",
         description: "Salsa pizza, queso muzzarella, rúcula",
-        price: "10000",
+        price: "1000",
       },
       {
         name: "QUESO AZUL",
         description: "Salsa pizza, queso azul, queso muzzarella",
-        price: "10000",
+        price: "1000",
       },
       {
         name: "JAMÓN Y MORRONES",
         description: "Salsa pizza, queso muzzarella, jamón y morrones",
-        price: "10000",
+        price: "1000",
       },
       {
         name: "MARGARITA",
         description:
           "Salsa pizza, queso muzzarella, rodajas de tomate y albahaca",
-        price: "10000",
+        price: "1000",
       },
       {
         name: "HERALDO",
         description: "Salsa pizza, queso muzzarella y camarones",
-        price: "10000",
+        price: "1100",
       },
     ],
     postres: [
       {
         name: "BOMBÓN SUIZO",
         description: "",
-        price: "10000",
+        price: "650",
       },
       {
         name: "ALMENDRADO",
         description: "",
-        price: "10000",
+        price: "650",
       },
       {
         name: "CROCANTE",
         description: "",
-        price: "10000",
+        price: "650",
       },
     ],
   },
@@ -210,158 +222,158 @@ export default {
       {
         name: "DRY MARTINI",
         description: "Gin london dry, vermú seco y aceituna",
-        price: "10000",
+        price: "750",
       },
       {
         name: "MARGARITA",
         description: "Tequila, triple sec, zumo de lima y sal",
-        price: "10000",
+        price: "800",
       },
       {
         name: "TOM COLLINS",
         description: "Old Tom Gin, zumo de lima, classic syrup y cereza",
-        price: "10000",
+        price: "800",
       },
       {
         name: "BLACK RUSSIAN",
         description: "Vodka y licor de café",
-        price: "10000",
+        price: "750",
       },
       {
         name: "FERNET",
         description: "Fernet branca con Cola",
-        price: "10000",
+        price: "800",
       },
       {
         name: "NEGRONI",
         description: "London Dry Gin, vermú rosso y Campari",
-        price: "10000",
+        price: "800",
       },
       {
         name: "ROB ROY",
         description: "Whisky escocés, vermú rosso, bitter angostura y cerezas",
-        price: "10000",
+        price: "850",
       },
       {
         name: "GANCIA BATIDO",
         description: "Gancia, zumo de lima y azúcar",
-        price: "10000",
+        price: "750",
       },
       {
         name: "GIN TONIC",
         description:
           "London Dry Gin, agua tónica y líma (cítricos, botánicos y frutos rojos)",
-        price: "10000",
+        price: "750",
       },
       {
         name: "CAIPIRINHA",
         description: "Cachaça, lima y azúcar",
-        price: "10000",
+        price: "750",
       },
       {
         name: "CAIPIROSKA",
         description: "Vodka, lima y azúcar",
-        price: "10000",
+        price: "750",
       },
       {
         name: "MOJITO",
         description:
           "Ron blanco, bitter angostura, lima, hierbabuena, soda y azúcar",
-        price: "10000",
+        price: "800",
       },
       {
         name: "APEROL SPRITZ",
         description: "Aperol, prosecco y zumo de naranja",
-        price: "10000",
+        price: "900",
       },
       {
         name: "DAIQUIRI FROZEN",
         description: "Ron blanco, fruta a elección y azúcar",
-        price: "10000",
+        price: "750",
       },
       {
         name: "CAMPARI ORANGE",
         description: "Campari y zumo de naranja",
-        price: "10000",
+        price: "800",
       },
       {
         name: "CUBA LIBRE",
         description: "Ron, gaseosa cola y lima",
-        price: "10000",
+        price: "750",
       },
     ],
     bebidasSinAlcohol: [
       {
         name: "GASEOSAS 350ML",
         description: "",
-        price: "10000",
+        price: "350",
       },
       {
         name: "AGUA",
         description: "Con o sin gas",
-        price: "10000",
+        price: "300",
       },
       {
         name: "AGUA SABORIZADA",
         description: "",
-        price: "10000",
+        price: "300",
       },
       {
         name: "JUGO DE NARANJA TROPICAL",
         description: "Naranja exprimida y maracuyá",
-        price: "10000",
+        price: "650",
       },
       {
         name: "LIMONADA",
         description: "Lima, hierbabuena, gengibre y azúcar",
-        price: "10000",
+        price: "650",
       },
       {
         name: "JUGO DE NARANJA",
         description: "Naranja exprimida",
-        price: "10000",
+        price: "550",
       },
       {
         name: "ANANÁ Y MENTA",
         description: "Licuado de ananá y menta",
-        price: "10000",
+        price: "650",
       },
     ],
     cervezasVinos: [
       {
         name: "CERVEZA HEINEKEN",
         description: "",
-        price: "10000",
+        price: "800",
       },
       {
         name: "CERVEZA GROLSCH",
         description: "",
-        price: "10000",
+        price: "800",
       },
       {
         name: "CERVEZA IMPERIAL",
         description: "",
-        price: "10000",
+        price: "750",
       },
       {
         name: "PINTA CERVEZA ARTESANAL",
         description: "",
-        price: "10000",
+        price: "550",
       },
       {
         name: "MEDIA PINTA CERVEZA ARTESANAL",
         description: "",
-        price: "10000",
+        price: "400",
       },
       {
         name: "VINOS",
         description: "Consultar",
-        price: "10000",
+        price: "",
       },
       {
         name: "WHISKEY",
         description: "Consultar",
-        price: "10000",
+        price: "",
       },
     ],
   },
