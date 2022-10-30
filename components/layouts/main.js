ac1ba1b0bc67af987c9ad4dcebed0d94af70884a
Facundo Perez Montalvo <facuperezm@facundos-mbp.home>
2022-10-30 14:13:36 -0300

    fix3

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,15 +1,13 @@
 import Head from "next/head";
-// import Footer from "../Footer";
 
 const Main = ({ children }) => {
   return (
     <div className="text-greenLoco bg-back-image bg-no-repeat bg-cover bg-center bg-fixed font-Roboto">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>LocoHeraldo</title>
+        <title>Loco Heraldo</title>
       </Head>
       <div>{children}</div>
-      {/* <Footer /> */}
     </div>
   );
 };
