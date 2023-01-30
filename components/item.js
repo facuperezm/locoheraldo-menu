1a8feeeaeb6a5e74df5b197f85c33299e0be5e6e
facuperezm <facundoperezmontalvo@gmail.com>
2023-01-30 14:14:19 -0300

    quickfix(app): fix on string and cursor pointer

--- components/item.js
+++ components/item.js
@@ -5,7 +5,7 @@ export default function Itemsito({ children }) {
     <div className="text-greenLoco my-2">
       <div className="flex justify-between font-black">
         <h3 className="uppercase">{children.name}</h3>
-        <span>${children.price}</span>
+        <span>{children.price || ""}</span>
       </div>
       <p className="normal-case">{children.description}</p>
     </div>
